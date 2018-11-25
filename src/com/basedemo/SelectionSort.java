41747412073a231f1d576f2beec59cd64f813f1c
wangbingzhigit <wbz853244870@outlook.com>
2018-11-26 00:25:41 +0800

    1

--- src/com/basedemo/SelectionSort.java
+++ src/com/basedemo/SelectionSort.java
@@ -2,7 +2,7 @@ package com.basedemo;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int a[]={12, 10, 11, 7, 6, 5, 4, 3, 2, 1};
+        int a[]={4,6,2,1};
 
         SelectionSort s = new SelectionSort();
         s.selectionSortTest(a);
@@ -11,15 +11,14 @@ public class SelectionSort {
         }
     }
     public void selectionSortTest(int a[]){
-        if (a==null||a.length<=0){
-            return;
-        }
         for(int i = 0; i < a.length -1; i++) {//比n-1趟
             int k = i;
             for (int j = k + 1; j < a.length; j++) {
                 //每次选择排序都从确定的最小的元素的后一个元素开始，与选定元素选择排序
+                int q = j;
                 if (a[j] < a[k]) {
                     k = j;
+                    System.out.println("最小值是"+a[k]);
                 }
             }
             if(i!=k){
