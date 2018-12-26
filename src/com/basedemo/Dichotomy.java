045cfd38b12a74b2068c4d9f66f85c3935f8528f
wangbingzhigit <wbz853244870@outlook.com>
2018-12-26 16:37:08 +0800

    1

--- src/com/basedemo/Dichotomy.java
+++ src/com/basedemo/Dichotomy.java
@@ -1,5 +1,7 @@
 package com.basedemo;
 
+import java.util.Scanner;
+
 /*
 二分法查找元素
 */
@@ -7,14 +9,17 @@ public class Dichotomy {
     public static void main(String[] args) {
         int[] list = {1, 3, 5, 7, 9, 10, 15, 18, 20};
         Dichotomy d = new Dichotomy();
-        int a = d.binary_search(list, 20);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入你要查找的数字：");
+        int item = sc.nextInt();
+        int a = d.binary_search(list, item);
         System.out.println(a);
     }
 
     public int binary_search(int[] list, int item) {
         int low = 0;
         int high = list.length - 1;
-        while (low <= high) {
+        while(low <= high) {
             int mid = (low + high) / 2;
             int guess = list[mid];
             if (guess == item) {
--- src/com/basedemo/QuickSort.java
+++ src/com/basedemo/QuickSort.java
@@ -2,21 +2,13 @@ package com.basedemo;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int a[]={4,2,6,1};                 //         2 1 4 6             1 2 4 6
+        int a[]={4,2,8,6,1,9};
         quick(a);
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i]+" ");
         }
     }
 
-    /**
-     * 查找出中轴（默认是最低位low）的在a数组排序后所在位置
-     *
-     * @param a 带查找数组
-     * @param low   开始位置
-     * @param high  结束位置
-     * @return  中轴所在位置
-     */
     public static int getMiddle(int[] a, int low,int high)
     {
         int flag = a[low]; //数组的第一个元素作为中轴
@@ -35,11 +27,7 @@ public class QuickSort {
         a[low] = flag ; //中轴记录到尾
         return low ; // 返回中轴的位置
     }
-/*
-     * @param numbers 带排序数组
-     * @param low  开始位置
-     * @param high 结束位置
-     */
+
     public static void quickSort(int[] a,int low,int high){
         if(low < high) {
             int middle = getMiddle(a,low,high); //将a数组进行一分为二
@@ -48,10 +36,7 @@ public class QuickSort {
         }
 
     }
-/*　
-     * 快速排序
-     * @param a 带排序数组
-     */
+
     public static void quick(int[] a) {
         if(a.length > 0){   //查看数组是否为空
             quickSort(a, 0, a.length-1);
